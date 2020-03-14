b6027dfcb22944ab3c8bac0886761bdbf8b93262
asheq <asheqosru@gmail.com>
2020-03-14 16:24:44 +0600

    an Activity and a method added

---
+++ app/src/main/java/com/example/myprojects/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.example.myprojects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void TeamB()
+    {
+
+        
+        Log.w("ghi","hello");
+    }
+}
